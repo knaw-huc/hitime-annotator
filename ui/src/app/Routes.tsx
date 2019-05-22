31bc864dd126e850629ef47d17d409b38c3d3adb
BasLee <basleenknegt@gmail.com>
2019-05-22 14:15:05 +0200

    Add page with items per term

--- ui/src/app/Routes.tsx
+++ ui/src/app/Routes.tsx
@@ -3,6 +3,7 @@ import {Redirect, Route, Switch, withRouter} from "react-router-dom";
 import AnnotatePage from "./annotate/AnnotatePage";
 import HomePage from "./home/HomePage";
 import TopTermsPage from "./items/TopTermsPage";
+import TermItemsPage from "./items/TermItemsPage";
 
 class Routes extends React.Component<any, any> {
     constructor(props: any, context: any) {
@@ -20,6 +21,7 @@ class Routes extends React.Component<any, any> {
                 <Route exact path='/annotate/' component={AnnotatePage} key={Math.random()}/>
                 <Route exact path='/home/' component={HomePage} key={pathname}/>
                 <Route exact path='/terms/' component={TopTermsPage} key={pathname}/>
+                <Route exact path='/terms/:tid/' component={TermItemsPage} key={pathname}/>
             </Switch>
         );
     }
--- ui/src/app/items/TermItemsPage.tsx
+++ ui/src/app/items/TermItemsPage.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-import Resources from "../Resources";
 import LoadingPage from "../common/LoadingPage";
+import InfoPage from "../common/InfoPage";
 import Page from "../common/Page";
 import MinimalPagination from "../common/MinimalPagination";
-import {Link, withRouter} from "react-router-dom";
-import InfoPage from "../common/InfoPage";
+import Resources from "../Resources";
 
-class TopTermsPage extends React.Component<any, any> {
+class TermItemsPage extends React.Component<any, any> {
     constructor(props: any, context: any) {
         super(props, context);
         this.state = {
@@ -14,16 +13,16 @@ class TopTermsPage extends React.Component<any, any> {
             from: 0,
             total: 0,
             size: 10,
-            terms: []
+            items: []
         };
-        this.getTopTerms();
+        this.getTermItems();
     }
 
-    private getTopTerms(from = this.state.from, size = this.state.size) {
-        Resources.getTerms(from, size).then((termsResponse) => {
+    private getTermItems(from = this.state.from, size = this.state.size) {
+        Resources.getTerm(this.props.match.params.tid, from, size).then((termsResponse) => {
             termsResponse.json().then((json) => {
                 this.setState({
-                    terms: json.frequencies,
+                    items: json.occurences,
                     from: from,
                     size: size,
                     total: json.total,
@@ -33,21 +32,24 @@ class TopTermsPage extends React.Component<any, any> {
         }).catch(() => this.setState({loading: false, error: "Could not get terms"}))
     }
 
-    private renderTerms() {
+    private renderItems() {
         return <ul className="list-group mt-3">
-            {this.state.terms.map((t: any, i: number) => {
+            {this.state.items.map((t: any, i: number) => {
                 return <li
                     key={i}
                     className="list-group-item list-group-item-action"
                 >
                     <div className="custom-control custom-radio">
-                        <span className="text-primary">{t.key}</span>
-                        <small className="text-secondary"> ({t.freq}x)</small>
+                        <span className="text-primary">{t.source}</span>
+                        {t.controlAccess
+                            ? <small className="text-secondary"> (control access)</small>
+                            : null
+                        }
                         <button
                             className="btn btn-success btn-sm float-right"
-                            onClick={() => this.props.history.push(`/terms/${t.key}/`)}
+                            onClick={() => this.props.history.push(`/`)}
                         >
-                            View
+                            Annotate
                             &nbsp;
                             <i className="fa fa-chevron-right"/>
                         </button>
@@ -68,19 +70,19 @@ class TopTermsPage extends React.Component<any, any> {
         let from = this.state.from;
 
         return (
-            <Page className="top-items">
-                <h2>Persons and Corporations</h2>
-                {this.renderTerms()}
+            <Page className="term-items">
+                <h2>All items of {this.props.match.params.tid}</h2>
+                {this.renderItems()}
                 {/* server is zero- and component one-based: */}
                 <MinimalPagination
                     page={from / size + 1}
                     lastPage={Math.ceil(this.state.total / size)}
-                    onPrevious={() => this.getTopTerms(from - size)}
-                    onNext={() => this.getTopTerms(from + size)}
+                    onPrevious={() => this.getTermItems(from - size)}
+                    onNext={() => this.getTermItems(from + size)}
                 />
             </Page>
         );
     }
 }
 
-export default withRouter(TopTermsPage);
+export default TermItemsPage;
